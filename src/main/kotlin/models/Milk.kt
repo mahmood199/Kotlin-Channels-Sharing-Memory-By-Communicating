1eb14774452f85c265e5789ac393fc06023066a6
mahmood <gaudentas199@gmail.com>
2023-05-01 05:48:43 +0530

    Converted sealed interfaces to sealed class with common "name" property

--- src/main/kotlin/models/Milk.kt
+++ src/main/kotlin/models/Milk.kt
@@ -1,8 +1,13 @@
 package models
 
-enum class Milk {
-    Whole,
-    Breve,
-    NonFat,
-    Steamed
+sealed class Milk(open val name: String) {
+
+    data class Whole (override val name: String = "Whole") : Milk(name)
+
+    data class Breve(override val name: String = "Breve") : Milk(name)
+
+    data class NonFat(override val name: String = "NonFat") : Milk(name)
+
+    data class SteamedMilk(override val name: String = "SteamedMilk") : Milk(name)
+
 }
